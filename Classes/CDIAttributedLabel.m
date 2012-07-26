da1a59d3f1ba7adeeb4a4d0e647ae42581f64f96
Sam Soffes <sam@samsoff.es>
2012-07-26 15:28:35 -0700

    Switch TTTAttributedLabel to ARC

--- Classes/CDIAttributedLabel.m
+++ Classes/CDIAttributedLabel.m
@@ -12,18 +12,18 @@
 @implementation CDIAttributedLabel
 
 - (void)drawStrike:(CTFrameRef)frame inRect:(CGRect)rect context:(CGContextRef)c {
-    NSArray *lines = (NSArray *)CTFrameGetLines(frame);
+    NSArray *lines = (__bridge NSArray *)CTFrameGetLines(frame);
     CGPoint origins[[lines count]];
     CTFrameGetLineOrigins(frame, CFRangeMake(0, 0), origins);
     
     CFIndex lineIndex = 0;
     for (id line in lines) {        
-        CGRect lineBounds = CTLineGetImageBounds((CTLineRef)line, c);
+        CGRect lineBounds = CTLineGetImageBounds((__bridge CTLineRef)line, c);
         lineBounds.origin.x = origins[lineIndex].x;
         lineBounds.origin.y = origins[lineIndex].y;
         
-        for (id glyphRun in (NSArray *)CTLineGetGlyphRuns((CTLineRef)line)) {
-            NSDictionary *attributes = (NSDictionary *)CTRunGetAttributes((CTRunRef) glyphRun);
+        for (id glyphRun in (__bridge NSArray *)CTLineGetGlyphRuns((__bridge CTLineRef)line)) {
+            NSDictionary *attributes = (__bridge NSDictionary *)CTRunGetAttributes((__bridge CTRunRef) glyphRun);
             BOOL strikeOut = [[attributes objectForKey:kTTTStrikeOutAttributeName] boolValue];
             NSInteger superscriptStyle = [[attributes objectForKey:(id)kCTSuperscriptAttributeName] integerValue];
             
@@ -32,10 +32,10 @@
                 CGFloat ascent = 0.0f;
                 CGFloat descent = 0.0f;
                 
-                runBounds.size.width = CTRunGetTypographicBounds((CTRunRef)glyphRun, CFRangeMake(0, 0), &ascent, &descent, NULL);
+                runBounds.size.width = CTRunGetTypographicBounds((__bridge CTRunRef)glyphRun, CFRangeMake(0, 0), &ascent, &descent, NULL);
                 runBounds.size.height = ascent + descent;
                 
-                CGFloat xOffset = CTLineGetOffsetForStringIndex((CTLineRef)line, CTRunGetStringRange((CTRunRef)glyphRun).location, NULL);
+                CGFloat xOffset = CTLineGetOffsetForStringIndex((__bridge CTLineRef)line, CTRunGetStringRange((__bridge CTRunRef)glyphRun).location, NULL);
                 runBounds.origin.x = origins[lineIndex].x + rect.origin.x + xOffset;
 				runBounds.origin.y = origins[lineIndex].y; // + rect.origin.y;
                 runBounds.origin.y -= descent;
@@ -60,7 +60,7 @@
                 id color = [attributes objectForKey:(id)kCTForegroundColorAttributeName];
 				
                 if (color) {
-                    CGContextSetStrokeColorWithColor(c, (CGColorRef)color);
+                    CGContextSetStrokeColorWithColor(c, (__bridge CGColorRef)color);
                 } else {
                     CGContextSetGrayStrokeColor(c, 0.0f, 1.0);
                 }
