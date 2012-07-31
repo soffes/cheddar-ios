e30cc78b61ac0f353d3173edcf0cbb7a2e394c98
Nicolas Gomollon <nicolasgomollon@techno-magic.com>
2012-07-30 22:51:16 -0300

    Fix settings selected row #33

--- Classes/CDISettingsTableViewCell.h
+++ Classes/CDISettingsTableViewCell.h
@@ -6,5 +6,10 @@
 //  Copyright (c) 2012 Nothing Magical. All rights reserved.
 //
 
-@interface CDISettingsTableViewCell : UITableViewCell
+@interface CDISettingsTableViewCell : UITableViewCell {
+	NSString *_cellIdentifier;
+}
+
+@property (nonatomic, strong) NSString *_cellIdentifier;
+
 @end
--- Classes/CDISettingsTableViewCell.m
+++ Classes/CDISettingsTableViewCell.m
@@ -6,14 +6,19 @@
 //  Copyright (c) 2012 Nothing Magical. All rights reserved.
 //
 
+#import <QuartzCore/QuartzCore.h>
 #import "CDISettingsTableViewCell.h"
 #import "UIColor+CheddariOSAdditions.h"
 #import "UIFont+CheddariOSAdditions.h"
 
 @implementation CDISettingsTableViewCell
 
+@synthesize _cellIdentifier;
+
 - (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier {
 	if ((self = [super initWithStyle:style reuseIdentifier:reuseIdentifier])) {
+		self._cellIdentifier = reuseIdentifier;
+		
 		self.backgroundColor = [UIColor whiteColor];
 		self.textLabel.font = [UIFont cheddarFontOfSize:17.0f];
 		self.textLabel.textColor = [UIColor cheddarTextColor];
@@ -35,6 +40,42 @@
 		self.selectedBackgroundView = selectedBackground;
 	}
 	return self;
+}
+
+- (void)drawRect:(CGRect)rect {
+	if (![self._cellIdentifier hasSuffix:@"None"]) {
+		UIView *selectedBackground = self.selectedBackgroundView;
+		
+		UIRectCorner cornersToRound;
+		if ([self._cellIdentifier hasSuffix:@"Top"]) cornersToRound = (UIRectCornerTopLeft | UIRectCornerTopRight);
+		if ([self._cellIdentifier hasSuffix:@"Bottom"]) cornersToRound = (UIRectCornerBottomLeft | UIRectCornerBottomRight);
+		if ([self._cellIdentifier hasSuffix:@"Both"]) cornersToRound = UIRectCornerAllCorners;
+		
+		float cellWidth = rect.size.width;
+		if (cellWidth > 480.0) cellWidth -= 60.0;
+		else cellWidth -= 18.0;
+		
+		float cellHeight = rect.size.height;
+		if ([self._cellIdentifier hasSuffix:@"Bottom"] || [self._cellIdentifier hasSuffix:@"Both"]) cellHeight -= 1.0;
+		
+		CGRect cellRect = CGRectMake(rect.origin.x, rect.origin.y, cellWidth, cellHeight);
+		
+		/* Rounding corners adapted from: http://stackoverflow.com/a/5826698 */
+		// Create the path
+		UIBezierPath *maskPath = [UIBezierPath bezierPathWithRoundedRect:cellRect
+													   byRoundingCorners:cornersToRound
+															 cornerRadii:CGSizeMake(10.0, 10.0)];
+		
+		// Create the shape layer and set its path
+		CAShapeLayer *maskLayer = [CAShapeLayer layer];
+		maskLayer.frame = cellRect;
+		maskLayer.path = maskPath.CGPath;
+		
+		// Set the newly created shape layer as the mask for the view's layer
+		selectedBackground.layer.mask = maskLayer;
+		self.selectedBackgroundView = selectedBackground;
+	}
+	[super drawRect:rect];
 }
 
 @end
